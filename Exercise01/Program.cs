7b5424a136288f43f5773f41acfbfd3498124f2d
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-22 12:27:53 +0700

    Add Exercise01

---
+++ Exercise01/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int, List<int>> ListOfPrimeSmallerX = (x) =>
+            {
+                var list = new List<int>();
+
+                for (int i = 2; i <= x - 1; i++)
+                {
+                    if (i.IsPrime())
+                    {
+                        list.Add(i);
+                    }
+                }
+
+                return list;
+            };
+
+            var newList = ListOfPrimeSmallerX(100);
+
+            newList.ForEach(Console.WriteLine);
+
+            Console.ReadKey();
+        }
+    }
+
+    public static class Extention
+    {
+        public static bool IsPrime(this int x)
+        {
+            var n = Math.Sqrt(x);
+
+            for (int i = 2; i <= n; i++)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}

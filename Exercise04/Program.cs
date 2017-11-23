732ddfa4ee7e3ddde6debe82cf7e67eefdeadffa
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-23 13:34:29 +0700

    Refactor code

--- Exercise04/Program.cs
+++ Exercise04/Program.cs
@@ -11,28 +11,28 @@ namespace Exercise04
     {
         static void Main(string[] args)
         {
-            Func<string, Func<string, bool>, List<string>> FilterFile = (path, endWith) =>
-            {
-                var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
-
-                var list = new List<string>();
-
-                foreach(var i in files)
-                {
-                    if (endWith(i))
-                    {
-                        list.Add(i);
-                    }
-                }
-
-                return list;
-            };
-
             var pathFolder = @"C:\Users\huynh\OneDrive";
             var newList = FilterFile(pathFolder, x =>x.EndsWith(".txt"));
             newList.ForEach(Console.WriteLine);
 
             Console.ReadKey();
+        }
+
+        public static List<string> FilterFile(string path, Func<string, bool> endWith)
+        {
+            var files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+
+            var list = new List<string>();
+
+            foreach (var i in files)
+            {
+                if (endWith(i))
+                {
+                    list.Add(i);
+                }
+            }
+
+            return list;
         }
     }
 }

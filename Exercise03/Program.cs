9b04adb1ecbd7a4d34e5a111ef7d80662f178c41
U-MINH\huynh <huynhngocminh2511@gmail.com>
2017-11-24 17:34:32 +0700

    Refactor code

--- Exercise03/Program.cs
+++ Exercise03/Program.cs
@@ -6,28 +6,26 @@ using System.Threading.Tasks;
 
 namespace Exercise03
 {
-    public delegate double Sum(double n, Func<double, double> method);
-
     class Program
     {
+        private static double Sum(double n, Func<double, double> method)
+        {
+            var sum = 0.0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum = sum + method(i);
+            }
+
+            return sum;
+        }
+
         static void Main(string[] args)
         {
-            Sum sumResult = (x, method) =>
-            {
-                var sum = 0.0;
-
-                for (int i = 1; i <= x; i++)
-                {
-                    sum = sum + method(i);
-                }
-
-                return sum;
-            };
-
             var n = 3;
 
-            Console.WriteLine(sumResult(n, x => Math.Pow(x, x)));
-            Console.WriteLine(sumResult(n, x => x * (x + 1)));
+            Console.WriteLine(Sum(n, x => Math.Pow(x, x)));
+            Console.WriteLine(Sum(n, x => x * (x + 1)));
 
             Console.ReadKey();
         }
